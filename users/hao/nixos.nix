e584bf933297a607da30c96c9d4451e9f43fb5b6
starry_tree <starry_tree@qq.com>
2026-07-05 16:11:10 +0800

    username

--- users/hao/nixos.nix
+++ users/hao/nixos.nix
@@ -5,10 +5,13 @@
   #
   ##################################################################################################################
 
-  users.users.ryan = {
-    # Ryan's authorizedKeys
+  users.users.hao = {
+    isNormalUser = true;
+    group = "hao";
     openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJx3Sk20pLL1b2PPKZey2oTyioODrErq83xG78YpFBoj ryan@ryan"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJx3Sk20pLL1b2PPKZey2oTyioODrErq83xG78YpFBoj hao@hao"
     ];
   };
+
+  users.groups.hao = {};
 }
